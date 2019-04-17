1992bb17b958b6325210d7e9ca18b9293f762308
mininet <Jkmo72>
2019-04-17 10:46:22 -0700

    Funciona

--- Topo_Proyecto.py
+++ Topo_Proyecto.py
@@ -1,11 +1,15 @@
-
+# Topologia Proyecto
 from mininet.topo import Topo
+
 class MyTopo( Topo ):
     "Simple topology example." 
+
   def __init__( self ):
         "Create custom topo."
+
         # Initialize topology
         Topo.__init__( self )
+
         # Add hosts and switches
         Host1 = self.addHost( 'h1' )
         Host2 = self.addHost( 'h2' )
@@ -14,11 +18,13 @@ class MyTopo( Topo ):
 	ServerWeb = self.addHost( 'SrvWeb' )	
         Switch1 = self.addSwitch( 's1' )
         Switch2 = self.addSwitch( 's2' )
+
         # Add links
         self.addLink( Host1, Switch1 )
         self.addLink( Host2, Switch1 )	
 	self.addLink( Host3, Switch1 )
         self.addLink( Switch1, Switch2 )
-       self.addLink( Switch2, Host4 )
+        self.addLink( Switch2, Host4 )
         self.addLink( Switch2, ServerWeb )
+
 topos = { 'mytopo': ( lambda: MyTopo() ) }
